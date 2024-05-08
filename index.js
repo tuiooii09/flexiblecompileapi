ae1e4391cd93020d459a0148990b71b9c392a7dd
tuiooii09 <chrisp9080pun0suy@hotmail.com>
2024-05-09 00:41:16 +0700

    demo commit

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-function moveZeroes(nums) {
-  let index = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      nums[index++] = nums[i];
-    }
-  }
-  for (let i = index; i < nums.length; i++) {
-    nums[i] = 0;
-  }
-  return nums;
+function isPalindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
 }
+
+console.log(isPalindrome("racecar")); // true
