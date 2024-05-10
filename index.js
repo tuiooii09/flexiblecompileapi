643f3de1280360883b5fe1a1584c87dcd798f982
tuiooii09 <chrisp9080pun0suy@hotmail.com>
2024-05-10 12:28:40 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function isPalindrome(str) {
-  const reversed = str.split("").reverse().join("");
-  return str === reversed;
+function isSubsequence(s, t) {
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) i++;
+    j++;
+  }
+  return i === s.length;
 }
-
-console.log(isPalindrome("racecar")); // true
