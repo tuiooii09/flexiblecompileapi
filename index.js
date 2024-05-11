fecee0dabccdbe039c038f1223077a253aa69935
tuiooii09 <chrisp9080pun0suy@hotmail.com>
2024-05-12 05:42:13 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const deepFlatten = (arr) =>
-  [].concat(...arr.map((v) => (Array.isArray(v) ? deepFlatten(v) : v)));
+function subarraySum(nums, k) {
+  const map = new Map();
+  map.set(0, 1);
+  let count = 0;
+  let sum = 0;
+  for (const num of nums) {
+    sum += num;
+    if (map.has(sum - k)) {
+      count += map.get(sum - k);
+    }
+    map.set(sum, (map.get(sum) || 0) + 1);
+  }
+  return count;
+}
