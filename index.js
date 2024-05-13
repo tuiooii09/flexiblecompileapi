452a1b100fb78914c28b65d9527e3488cdbb8285
tuiooii09 <chrisp9080pun0suy@hotmail.com>
2024-05-13 08:14:56 +0700

    test task

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const quickSort = (arr) => {
-  if (arr.length <= 1) {
-    return arr;
-  }
-  const pivot = arr[arr.length - 1];
-  const left = [];
-  const right = [];
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] < pivot) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
+function countPrimes(n) {
+  const isPrime = new Array(n).fill(true);
+  isPrime[0] = false;
+  isPrime[1] = false;
+  for (let i = 2; i * i < n; i++) {
+    if (isPrime[i]) {
+      for (let j = i * i; j < n; j += i) {
+        isPrime[j] = false;
+      }
     }
   }
-  return quickSort(left).concat(pivot, quickSort(right));
-};
+  let count = 0;
+  for (let i = 2; i < n; i++) {
+    if (isPrime[i]) count++;
+  }
+  return count;
+}
