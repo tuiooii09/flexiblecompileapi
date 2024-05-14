2e30653f0fa10fe63a8ae5759752a1a96ec37ab6
tuiooii09 <chrisp9080pun0suy@hotmail.com>
2024-05-14 18:24:38 +0700

    start project

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-function countPrimes(n) {
-  const isPrime = new Array(n).fill(true);
-  isPrime[0] = false;
-  isPrime[1] = false;
-  for (let i = 2; i * i < n; i++) {
-    if (isPrime[i]) {
-      for (let j = i * i; j < n; j += i) {
-        isPrime[j] = false;
-      }
-    }
+function maxProduct(nums) {
+  if (nums.length === 0) return 0;
+  let maxSoFar = nums[0];
+  let minSoFar = nums[0];
+  let maxProduct = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    const temp = maxSoFar;
+    maxSoFar = Math.max(nums[i], nums[i] * maxSoFar, nums[i] * minSoFar);
+    minSoFar = Math.min(nums[i], nums[i] * temp, nums[i] * minSoFar);
+    maxProduct = Math.max(maxProduct, maxSoFar);
   }
-  let count = 0;
-  for (let i = 2; i < n; i++) {
-    if (isPrime[i]) count++;
-  }
-  return count;
+  return maxProduct;
 }
